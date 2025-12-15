6adf7f2641b6c35ef5ab4bdbed1807b6dfb6f42c
Eric Minas <s8ermina@stud.uni-saarland.de>
2025-12-15 15:13:16 +0100

    wip fix pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// https://vite.dev/config/
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import { playwright } from "@vitest/browser-playwright";
+import fs from "node:fs";
+
 const dirname =
 	typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url));
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+
 export default defineConfig({
 	plugins: [
 		react({
@@ -16,20 +17,24 @@ export default defineConfig({
 				plugins: [["babel-plugin-react-compiler"]],
 			},
 		}),
+		// Plugin to create .nojekyll file
+		{
+			name: "create-nojekyll",
+			closeBundle() {
+				fs.writeFileSync(path.resolve(dirname, "dist/.nojekyll"), "");
+			},
+		},
 	],
-	base: "/SimpleGraphCreator/", // Move it here at the root level
+	base: "/SimpleGraphCreator/",
 	test: {
 		projects: [
 			{
 				extends: true,
 				plugins: [
-					// The plugin will run tests for the stories defined in your Storybook config
-					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
 					storybookTest({
 						configDir: path.join(dirname, ".storybook"),
 					}),
 				],
-				// Remove base from here
 				test: {
 					name: "storybook",
 					browser: {
